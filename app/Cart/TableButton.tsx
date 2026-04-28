2cf85d3fa8d515208b8137d7b70e04f691ed7113
Elnq01 <qele95@gmail.com>
2026-04-28 12:26:40 +0100

    type issues

--- app/Cart/TableButton.tsx
+++ app/Cart/TableButton.tsx
@@ -2,20 +2,29 @@ import { Primary } from "@/public/colors/colos";
 import { Button } from "react-bootstrap";
 import { FaTrash } from "react-icons/fa";
 
-export default function TableButton({titled, onClick}:{titled:string, onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;}){
-    return <Button 
-               onClick={onClick}
-                style={{
-                  display:'flex', 
-                  flexDirection:'row', 
-                  alignItems:'center', 
-                  textAlign:'center',
-                  columnGap:'10px',
-                  color:Primary,
-                  background:"rgb(214, 243, 216)",
-                  border:'none'
-                  }}>
-                  <FaTrash size={20} /> 
-                  <p>{titled}</p>
-              </Button>
+export default function TableButton({
+  titled,
+  onClick,
+}: {
+  titled: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <Button
+      onClick={onClick}
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        textAlign: "center",
+        columnGap: "10px",
+        color: Primary,
+        background: "rgb(214, 243, 216)",
+        border: "none",
+      }}
+    >
+      <FaTrash size={20} />
+      <p>{titled}</p>
+    </Button>
+  );
 }
